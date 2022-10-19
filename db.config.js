2287503ed760b0331aa6786535304dd63786c44a
CorentinP1234 <corentin.prad@gmail.com>
2022-10-19 17:02:51 -0400

    final

--- db.config.js
+++ db.config.js
@@ -1,8 +1,23 @@
+// module.exports = {
+//     HOST: "127.0.0.1",
+//     PORT: 3306,
+//     USER: "root",
+//     PASSWORD: "Password",
+//     DB: "first_db",
+//     dialect: "mysql",
+//     pool: {
+//         max: 5,
+//         min: 0,
+//         acquire: 30000,
+//         idle: 10000
+//     }
+// };
+
 module.exports = {
-    HOST: "127.0.0.1",
+    HOST: "mylibrary.mysql.database.azure.com",
     PORT: 3306,
-    USER: "root",
-    PASSWORD: "Password",
+    USER: "rootmylibrary",
+    PASSWORD: "testtest1!",
     DB: "first_db",
     dialect: "mysql",
     pool: {
